c32ca5234a123f619e8def8ebede855e262c422b
SE00808374@techmahindra.com <SE00808374@techmahindra.com>
2022-01-25 09:36:56 +0000

    Migrate legacy code for customer_orders

---
+++ models/marts/fct_customer_orders.sql
@@ -0,0 +1,126 @@
+with
+
+orders as (
+
+    select * from {{ ref('int_orders')}}
+),
+
+customers as (
+
+    select * from {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
+
+
+---
+order_totals as (
+
+    select
+
+      order_id,
+      payment_status,
+      sum(payment_amount) as order_value_dollars
+
+    from payments
+    group by 1,2
+),
+
+order_values_joined as (
+
+    select
+
+      orders.*,
+      order_totals.payment_status,
+      order_totals.order_value_dollars
+
+
+    from orders
+    left join order_totals
+      on orders.order_id = order_totals.order_id
+),
+
+---
+customer_order_history as (
+
+    select 
+        customers.customer_id,
+        customers.full_name,
+        customers.surname,
+        customers.givenname,
+
+        min(orders.order_date) as first_order_date,
+
+        min(orders.valid_order_date) as first_non_returned_order_date,
+
+        max(orders.valid_order_date) as most_recent_non_returned_order_date,
+
+        coalesce(max(orders.user_order_seq),0) as order_count,
+
+        coalesce(count(case 
+            when orders.valid_order_date is not null
+            then 1 end),
+            0
+        ) as non_returned_order_count,
+
+        sum(case 
+            when orders.valid_order_date is not null
+            then orders.order_value_dollars 
+            else 0 
+        end) as total_lifetime_value,
+
+        sum(case
+            when orders.valid_order_date is not null
+            then orders.order_value_dollars 
+            else 0 
+        end)
+        / nullif(count(case 
+            when orders.valid_order_date is not null 
+            then 1 end),
+            0
+        ) as avg_non_returned_order_value,
+
+        array_agg(distinct orders.order_id) as order_ids
+
+    from orders
+
+    join customers
+    on orders.customer_id = customers.customer_id
+
+    
+
+
+    group by customers.customers_id, customers.full_name, customers.surname, customers.givenname
+
+),
+
+-- Final CTEs 
+final as (
+
+    select 
+
+        orders. order_id,
+        orders.customer_id,
+        customers.surname,
+        customers.givenname,
+        first_order_date,
+        order_count,
+        total_lifetime_value,
+        orders.order_value_dollars,
+        orders.order_status,
+        orders.payment_status
+
+    from orders
+
+    join customers
+    on orders.customer_id = customers.customer_id
+
+    join customer_order_history
+    on orders.customer_id = customer_order_history.customer_id
+
+    
+
+)
+
+-- Simple Select Statement
+select * from final
